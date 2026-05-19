b879903aac3ff3ed1a76e07bb960e8d22e7f0191
welele94 <2400005@estudante.uab.pt>
2026-05-19 10:50:19 +0100

    Adicionar base Prolog inicial

---
+++ base_conhecimento.pl
@@ -0,0 +1,32 @@
+:- dynamic aluno/2.
+:- dynamic forum/2.
+:- dynamic media/2.
+:- dynamic estado/2.
+
+aluno(1, 'Ana').
+aluno(2, 'Bruno').
+aluno(3, 'Carla').
+aluno(4, 'Diogo').
+aluno(5, 'Eva').
+aluno(6, 'Filipe').
+
+forum(1, 4).
+forum(2, 1).
+forum(3, 5).
+forum(4, 3).
+forum(5, 2).
+forum(6, 6).
+
+media(1, 13.5).
+media(2, 8.5).
+media(3, 17.5).
+media(4, 10.5).
+media(5, 8.5).
+media(6, 15.0).
+
+estado(1, aprovado).
+estado(2, em_risco).
+estado(3, aprovado).
+estado(4, condicionado).
+estado(5, em_risco).
+estado(6, aprovado).
